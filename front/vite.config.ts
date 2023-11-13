dbba72a95dee6400ed50a9ec0174ecd0ad0f581d
npldevfr <asnpldev@gmail.com>
2023-11-13 17:05:52 +0100

    feat: docker

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    port: 8000,
+    watch: {
+      usePolling: true
+    }
+  }
 })
