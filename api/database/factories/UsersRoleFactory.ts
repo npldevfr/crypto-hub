f4df32cff8814d50a1ce62cf103ce59c8b6c485f
baptiste <baptiste.pgn@gmail.com>
2023-11-21 12:33:20 +0100

    feat(database, models): generate schema, controller and factory

---
+++ api/database/factories/UsersRoleFactory.ts
@@ -0,0 +1,8 @@
+import users_roles from 'App/Models/users_roles'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+
+export default Factory.define(users_roles, ({ faker }) => {
+  return {
+    //
+  }
+}).build()
