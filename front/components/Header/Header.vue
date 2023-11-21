a843a7cd3ab376b2b6ac076d47ca9ee1fd74ca34
H4ppyCode ~ Pacôme <dhiser.pacome@gmail.com>
2023-11-21 16:51:11 +0100

    fix(header,avatar.vue): fix navbar

--- front/components/Header/Header.vue
+++ front/components/Header/Header.vue
@@ -1,30 +1,57 @@
+<script lang="ts" setup>
+const isLogged = ref(false);
+</script>
 <template>
-  <div class="flex flex-row w-full">
-    <div class="flex mx-20 w-full">
+  <div class="flex flex-row px-20 w-full items-center">
+    <div class="flex lg-w-75">
       <Logo size="sm" />
     </div>
-    <div class="flex mx-40 p-5 space-x-18 w-full">
-      <a
-        href="#"
-        class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-        aria-current="page"
-        >Dashboard</a
+    <div class="flex">
+      <input
+        type="text"
+        id="input-group-1"
+        class="bg-gray-50 w-65 h-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Rechercher ..."
+        style="text-align: center"
+      />
+    </div>
+    <div class="flex p-5 space-x-10 w-full">
+      <NuxtLink
+        to="/"
+        class="text-white bg-gray-900 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+        >Accueil</NuxtLink
       >
-      <a
-        href="#"
+      <NuxtLink
+        to="/"
         class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-        >Team</a
+        >Actualités</NuxtLink
       >
-      <a
-        href="#"
+      <NuxtLink
+        to="/"
         class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-        >Projects</a
+        >Crypto</NuxtLink
       >
-      <a
-        href="#"
-        class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-        >Calendar</a
+    </div>
+    <div class="flex flex-row justify-end items-center space-x-10 w-full">
+      <div v-if="isLogged">
+        <ProfileAvatar />
+      </div>
+      <div
+        v-else
+        class="flex flex-row justify-end items-center space-x-10 w-full"
       >
+        <NuxtLink
+          to="/login"
+          class="text-gray-300 flex flex-shrink-0 hover:bg-gray-700 hover:text-white rounded-md px-6 py-2 text-sm font-medium"
+          >Se connecter</NuxtLink
+        >
+
+        <NuxtLink
+          to="/register"
+          class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          >S'inscrire</NuxtLink
+        >
+      </div>
     </div>
   </div>
 </template>
---
+++ front/components/Profile/Avatar.vue
@@ -0,0 +1,64 @@
+<script lang="ts" setup>
+const hasAvatar = ref(true);
+</script>
+
+<template>
+  <div class="w-10 h-10 rounded-full cursor-pointer">
+    <template v-if="hasAvatar">
+      <img
+        id="avatarButton"
+        type="button"
+        data-dropdown-toggle="userDropdown"
+        data-dropdown-placement="bottom-start"
+        src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/655.jpg"
+        alt="User dropdown"
+        class="rounded-full"
+      />
+    </template>
+    <template v-else>
+      <div>Par défaut</div>
+    </template>
+  </div>
+  <div
+    id="userDropdown"
+    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+  >
+    <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+      <div>Bonnie Green</div>
+      <div class="font-medium truncate">name@flowbite.com</div>
+    </div>
+    <ul
+      class="py-2 text-sm text-gray-700 dark:text-gray-200"
+      aria-labelledby="avatarButton"
+    >
+      <li>
+        <a
+          href="#"
+          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >Dashboard</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >Settings</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >Earnings</a
+        >
+      </li>
+    </ul>
+    <div class="py-1">
+      <a
+        href="#"
+        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+        >Sign out</a
+      >
+    </div>
+  </div>
+</template>
