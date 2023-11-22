db598fe767e6d3f1b037cc6030c36558cb156a93
DHISER Pacôme <dhiser.pacome@gmail.com>
2023-11-22 16:05:31 +0100

    feat(CardNews, index): add CardNews destructuring example

---
+++ front/components/CardNews/CardNews.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    verticalLayout: boolean;
+    imageUrl: string;
+    title: string;
+    description: string;
+    tags?: string[];
+    additionalInfo?: string;
+    avatarUrl?: string;
+    authorName?: string;
+    date?: string;
+  }>(),
+  {
+    verticalLayout: true,
+    tags: [],
+    additionalInfo: "",
+    avatarUrl: "",
+    authorName: "",
+    date: "",
+  }
+);
+</script>
+
+<template>
+  <div v-if="verticalLayout" class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full" :src="imageUrl" alt="Image de la carte" />
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ title }}</div>
+      <p class="text-gray-700 text-base">{{ description }}</p>
+      <p class="text-gray-900 mt-3 leading-none" v-if="authorName">
+        {{ authorName }}
+      </p>
+      <p class="text-gray-600" v-if="date">{{ date }}</p>
+
+      <p class="text-sm text-gray-600 px-3 py-3 flex items-center">
+        {{ additionalInfo }}
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span
+        v-for="tag in tags"
+        :key="tag"
+        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+      >
+        {{ tag }}
+      </span>
+    </div>
+  </div>
+
+  <div v-else class="max-w-sm w-full lg:max-w-full lg:flex">
+    <div
+      class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+      :style="`background-image: url('${imageUrl}')`"
+      :title="additionalInfo"
+    ></div>
+    <div
+      class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+    >
+      <div class="mb-8">
+        <p class="text-sm text-gray-600 flex items-center">
+          {{ additionalInfo }}
+        </p>
+        <div class="text-gray-900 font-bold text-xl mb-2">{{ title }}</div>
+        <p class="text-gray-700 text-base">{{ description }}</p>
+      </div>
+      <div class="flex items-center" v-if="avatarUrl">
+        <div class="text-sm">
+          <p class="text-gray-900 leading-none" v-if="authorName">
+            {{ authorName }}
+          </p>
+          <p class="text-gray-600" v-if="date">{{ date }}</p>
+          <div class="px-6 pt-4 pb-2">
+            <span
+              v-for="tag in tags"
+              :key="tag"
+              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+            >
+              {{ tag }}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -4,5 +4,40 @@ const dd = useRouter();
 
 <template>
   <Header />
-  <div class="bg-red-200">hello !</div>
+  <div class="px-20 py-15">
+    <CardNews
+      :verticalLayout="false"
+      imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQARfTOMEBVV1_3fc3Ga_9E_UZ1pBnSGRHCG-qkB-3czg&s"
+      title="Titre de la carte horizontale"
+      description="Description de la carte horizontale"
+      additionalInfo="Information supplémentaire"
+      avatarUrl="https://exemple.com/avatar.jpg"
+      authorName="Nom de l'auteur"
+      date="Date de publication"
+      :tags="['#photography', '#travel', '#winter']"
+    />
+  </div>
+
+  <div class="flex flex-row space-x-10 px-20 py-10">
+    <CardNews
+      :verticalLayout="true"
+      imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQARfTOMEBVV1_3fc3Ga_9E_UZ1pBnSGRHCG-qkB-3czg&s"
+      title="The Coldest Sunset"
+      date="Date de publication"
+      authorName="Nom de l'auteur"
+      additionalInfo="Information supplémentaire"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+      :tags="['#photography', '#travel', '#winter']"
+    />
+    <CardNews
+      :verticalLayout="true"
+      imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQARfTOMEBVV1_3fc3Ga_9E_UZ1pBnSGRHCG-qkB-3czg&s"
+      title="The Coldest Sunset"
+      date="Date de publication"
+      authorName="Nom de l'auteur"
+      additionalInfo="Information supplémentaire"
+      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+      :tags="['#photography', '#travel', '#winter']"
+    />
+  </div>
 </template>
