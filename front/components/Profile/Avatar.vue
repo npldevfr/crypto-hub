e05c48ce1696ba1ee1b8cf1771c45886c1ddeb38
H4ppyCode ~ Pacôme <dhiser.pacome@gmail.com>
2023-11-27 14:04:12 +0100

    refactor(avatar,profileitem,profilenavigation): add profile's components

--- front/components/Profile/Avatar.vue
+++ front/components/Profile/Avatar.vue
@@ -27,38 +27,9 @@ const hasAvatar = ref(true);
       <div>Bonnie Green</div>
       <div class="font-medium truncate">name@flowbite.com</div>
     </div>
-    <ul
-      class="py-2 text-sm text-gray-700 dark:text-gray-200"
-      aria-labelledby="avatarButton"
-    >
-      <li>
-        <a
-          href="#"
-          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-          >Dashboard</a
-        >
-      </li>
-      <li>
-        <a
-          href="#"
-          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-          >Settings</a
-        >
-      </li>
-      <li>
-        <a
-          href="#"
-          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-          >Earnings</a
-        >
-      </li>
-    </ul>
-    <div class="py-1">
-      <a
-        href="#"
-        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-        >Sign out</a
-      >
-    </div>
+    <ProfileNavigation>
+      <ProfileItem to="/" label="Home" />
+      <ProfileItem to="/dashboard" label="Dashboard" />
+    </ProfileNavigation>
   </div>
 </template>
