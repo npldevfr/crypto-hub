99442c53746f7b4855cdf431a0cced97d4b0c646
Yannis <yannis.winkler@epitech.eu>
2023-12-11 12:33:12 +0100

    style: add group of routes

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -20,31 +20,34 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 import UsersController from '../app/Controllers/Http/UsersController'
+Route.group(() => {
+  Route.get('/profile', async (ctx) => {
+    return new UsersController().profile(ctx)
+  })
+
+  Route.post('/login', async (ctx) => {
+    return new UsersController().login(ctx)
+  })
+
+  Route.post('/logout', async (ctx) => {
+    return new UsersController().logout(ctx)
+  })
+
+  Route.post('/register', async (ctx) => {
+    return new UsersController().register(ctx)
+  })
+
+  Route.put('/profile', async (ctx) => {
+    return new UsersController().update(ctx)
+  })
+}).prefix('/api/users')
 
 Route.get('/', async () => {
   return { hi: 'coucou from API !' }
 })
 
-Route.get('/api/', async () => {
+Route.get('/api', async () => {
   return { hi: 'the Server is running !' }
 })
 
-Route.get('/api/users/profile', async (ctx) => {
-  return new UsersController().profile(ctx)
-})
 
-Route.post('/api/users/login', async (ctx) => {
-  return new UsersController().login(ctx)
-})
-
-Route.post('/api/users/logout', async (ctx) => {
-  return new UsersController().logout(ctx)
-})
-
-Route.post('/api/users/register', async (ctx) => {
-  return new UsersController().register(ctx)
-})
-
-Route.put('/api/users/profile', async (ctx) => {
-  return new UsersController().update(ctx)
-})
