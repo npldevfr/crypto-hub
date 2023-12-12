6a66a444d820e577f42970e71aad8cac31a692b9
npldevfr <asnpldev@gmail.com>
2023-12-12 23:56:37 +0100

    feat: dropdown

---
+++ front/components/Dropdown/Item.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {DropdownMenuItem} from "radix-vue";
+
+const { title, icon, handle } = defineProps<{
+  icon?: string
+  title: string
+  handle?: () => void
+}>()
+</script>
+
+<template>
+  <DropdownMenuItem
+      :text-value="title.toLowerCase()"
+      class="relative data-[disabled]:pointer-events-none pl-[30px] group data-[disabled]:opacity-50 mx-1 first-of-type:mt-1 last-of-type:mb-1 px-2 py-1.5 text-[13.5px] font-500 outline-none cursor-pointer rounded-[2px] transition-all hover:bg-stone-100/70  flex items-center gap-3"
+      @click="handle"
+  >
+    <div class="absolute left-0 top-0 transition-all duration-200 text-stone-900/50 group-hover:text-stone-900 w-[32px] h-[32px] flex items-center justify-center">
+      <div :class="icon" />
+    </div>
+    {{ title }}
+  </DropdownMenuItem>
+</template>
+
--- front/components/Header/Header.vue
+++ front/components/Header/Header.vue
@@ -3,39 +3,41 @@ import Avatar from "~/components/Avatar/Avatar.vue";
 import {headerService} from "~/services/header.service";
 
 const router = useRouter();
-const { getHeaderData } = headerService();
-const { data: headerData } = getHeaderData()
-const { isLogged, user } = useAuth()
+const {getHeaderData} = headerService();
+const {data: headerData} = getHeaderData()
+const {isLogged, user} = useAuth()
 
 </script>
 <template>
   <div class="flex flex-row border-b px-30 w-full h-[60px] items-center">
     <div class="flex lg-w-75">
-      <Logo size="sm" />
+      <Logo size="sm"/>
     </div>
 
     <div class="w-full">
-      <Navigation :header-data="headerData" >
+      <Navigation :header-data="headerData">
         <NavigationLink title="Actualités">
-            <NavigationTabsArticle />
+          <NavigationTabsArticle/>
         </NavigationLink>
         <NavigationLink title="Marché">
-          <NavigationTabsCryptocurrenciesList />
+          <NavigationTabsCryptocurrenciesList/>
         </NavigationLink>
-        <NavigationLink title="NFT" />
-        <NavigationLink title="Convertisseur" />
+        <NavigationLink title="NFT"/>
+        <NavigationLink title="Convertisseur"/>
       </Navigation>
     </div>
     <div class="flex flex-row justify-end items-center space-x-10 ">
       <div v-if="isLogged">
-        <Avatar :src="user?.avatar"
-                interactive
-                size="lg"
-                :alt="user?.username" />
+        <Dropdown>
+          <Avatar :src="user?.avatar"
+                  interactive
+                  size="lg"
+                  :alt="user?.username"/>
+        </Dropdown>
       </div>
       <div
-        v-else
-        class="flex flex-row justify-end items-center space-x-2 w-full"
+          v-else
+          class="flex flex-row justify-end items-center space-x-2 w-full"
       >
         <Button size="sm" hierarchy="link" @click="router.push('/register')">
           S'inscrire
