b54257c182c162fee718ea36efa55acd3941f0ac
npldevfr <asnpldev@gmail.com>
2023-12-18 15:15:44 +0100

    fix: generate manifest

--- api/rss/rssFeedUpdater.ts
+++ api/rss/rssFeedUpdater.ts
@@ -1,6 +1,6 @@
 import Parser from 'rss-parser'
-import Article from '../app/Models/Article'
-import ArticleSource from '../app/Models/ArticleSource'
+import Article from 'App/Models/Article'
+import ArticleSource from 'App/Models/ArticleSource'
 import NoTitleException from '../app/Exceptions/Rss/NoTilteExeptionException'
 import { JSDOM } from 'jsdom'
 
