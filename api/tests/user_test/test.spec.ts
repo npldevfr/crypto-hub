32654417fbd4bf6ab1136851d4a0a7e3af2b8270
Yannis <yannis.winkler@epitech.eu>
2023-12-18 10:41:35 +0100

    feat: add test for profile API

--- api/tests/user_test/test.spec.ts
+++ api/tests/user_test/test.spec.ts
@@ -1,4 +1,5 @@
 import { test } from '@japa/runner'
+import User from '../../app/Models/User'
 
 test.group('User', async (): Promise<void> => {
 
@@ -6,16 +7,38 @@ test.group('User', async (): Promise<void> => {
     await client.get('/')
   })
 
-  test('The user must not login', async ({ client/*, assert*/ }) => {
+  test('The user must not login', async ({ client }) => {
     const admin = { email: 'admin@gmail.com', password: 'wrong-password' }
     const response = await client.post('/api/users/login').form(admin)
     response.assertStatus(400)
-    // console.log(response.body())
   })
 
-  test('can the user login', async ({ client/*, assert*/ }) => {
+  test('can the user login', async ({ client }) => {
     const admin = { email: 'admin@gmail.com', password: 'admin' }
     const response = await client.post('/api/users/login').form(admin)
     response.assertStatus(200)
   })
+
+  test('get the profile of the connected users with the good structure', async ({ client, assert }) => {
+    const user = await User.findBy('email', "admin@gmail.com")
+    const response = await client.get('/api/users/profile').loginAs(user)
+    const body = await response.body()
+
+    const expectedStructure = {
+      id: '',
+      username: '',
+      email: '',
+      avatar: null,
+      is_blocked: false,
+      remember_me_token: null
+
+    };
+    response.assertStatus(200)
+    const extraFields = Object.keys(body).filter((field) => !(field in expectedStructure));
+    assert.deepEqual(extraFields, [], `Unexpected fields found: ${extraFields.join(', ')}`);
+
+    Object.keys(expectedStructure).forEach((field) => { assert.property(body, field); });
+
+  })
+
 })
