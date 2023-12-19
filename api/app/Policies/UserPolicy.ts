203f8d9ab9d8b01a5fc9f438cb1aaa226539c105
npldevfr <asnpldev@gmail.com>
2023-12-19 16:28:10 +0100

    feat: roles enum

--- api/app/Policies/UserPolicy.ts
+++ api/app/Policies/UserPolicy.ts
@@ -1,28 +1,29 @@
 import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
 import type User from 'App/Models/User'
+import { Roles } from '../../enums/Roles'
 
 export default class UserPolicy extends BasePolicy {
   public async view(user: User) {
-    return user.hasRoles(['administrator'])
+    return user.hasRoles([Roles.ADMINISTRATOR])
   }
 
   public async viewList(user: User) {
-    return user.hasRoles(['administrator'])
+    return user.hasRoles([Roles.ADMINISTRATOR])
   }
 
   public async update(user: User) {
-    return user.hasRoles(['administrator'])
+    return user.hasRoles([Roles.ADMINISTRATOR])
   }
 
   public async destroy(user: User) {
-    return user.hasRoles(['administrator'])
+    return user.hasRoles([Roles.ADMINISTRATOR])
   }
 
   public async block(user: User) {
-    return user.hasRoles(['administrator'])
+    return user.hasRoles([Roles.ADMINISTRATOR])
   }
 
   public async loginAs(user: User) {
-    return user.hasRoles(['administrator'])
+    return user.hasRoles([Roles.ADMINISTRATOR])
   }
 }
