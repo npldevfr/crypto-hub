433e7262650376c5495b91fa2a4d6b3b85a03063
Pacôme <dhiser.pacome@gmail.com>
2024-01-07 21:53:39 +0100

    feat: add searchbar in article page

--- front/pages/article/index.vue
+++ front/pages/article/index.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
+import { ref, computed } from "vue";
 import { articleServiceController } from "~/services/articleServiceController";
 
 const { index } = articleServiceController();
 const { data: articles } = index();
+
+const searchQuery = ref("");
+
+const filteredArticles = computed(() => {
+  if (!articles.value) return [];
+  return articles.value.filter((article) =>
+    article.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+  );
+});
 </script>
 
 <template>
-  <div v-if="articles" class="px-20 py-15">
+  <div class="search-bar-container">
+    <input
+      type="text"
+      v-model="searchQuery"
+      placeholder="Rechercher un article..."
+      class="search-input"
+    />
+  </div>
+
+  <div v-if="filteredArticles" class="px-20 py-15">
     <NuxtLink
-      v-for="(article, index) in articles"
+      v-for="(article, index) in filteredArticles"
       :key="index"
       :to="`/article/${article.slug}`"
-      ><CardNews
+    >
+      <CardNews
         :title="article.name"
         :vertical-layout="false"
         image-url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQARfTOMEBVV1_3fc3Ga_9E_UZ1pBnSGRHCG-qkB-3czg&s"
@@ -25,7 +45,7 @@ const { data: articles } = index();
 
   <div class="flex flex-row space-x-10 px-20 py-10">
     <NuxtLink
-      v-for="(article, index) in articles"
+      v-for="(article, index) in filteredArticles"
       :key="index"
       :to="`/article/${article.slug}`"
     >
@@ -42,3 +62,21 @@ const { data: articles } = index();
     </NuxtLink>
   </div>
 </template>
+
+<style>
+.search-bar-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 20px 0;
+}
+
+.search-input {
+  width: 50%; /* Ajustez la largeur selon vos besoins */
+  padding: 10px;
+  font-size: 16px; /* Ajustez la taille de police si nécessaire */
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+</style>
